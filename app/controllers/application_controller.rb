bb1eb4d585eb8eaba0ade0324a34c05c2a125353
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-03-28 12:28:43 +0000

    [fixing]facility

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  
+ before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    case resource
+    # when Customer
+      # root_path
+    when facility
+      admin_orders_path
+    end
+  end
+  
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      new_customer_session_path
+    end
+  end
+  
 end
